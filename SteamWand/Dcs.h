04ffaedd187f41ef4f52f4cc14ec434d901047a0
BlushyTears <m123.123.123@hotmail.com>
2026-02-28 04:20:44 +0100

    Restructure entirely

--- SteamWand/Dcs.h
+++ SteamWand/Dcs.h
@@ -1,47 +1,145 @@
-#pragma once
-
 #include <array>
 #include <bitset>
-#include <typeinfo>
-#include <unordered_map>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <tuple>
+#include <type_traits>
 
-constexpr int MAX_ITEMS = 1000;
+#include <vector>
 
-struct DataBlock {
-	std::bitset<MAX_ITEMS> Data;
-	// Philosophy: This should only be utilized if we misscalculate space needed
-	DataBlock* next = nullptr;
+struct Vec2 { float x, y; };
+struct Vec3 { float x, y, z; };
+
+using AtomTypes = std::tuple<int32_t, float, Vec2, Vec3, int>;
+
+template<typename T, typename Tuple, size_t I = 0>
+constexpr size_t type_index() {
+    if constexpr (I >= std::tuple_size_v<Tuple>) {
+        static_assert(I < std::tuple_size_v<Tuple>, "T not in type list");
+        return -1;
+    }
+    else if constexpr (std::is_same_v<T, std::tuple_element_t<I, Tuple>>) {
+        return I;
+    }
+    else {
+        return type_index<T, Tuple, I + 1>();
+    }
+}
+
+template<typename T>
+constexpr uint8_t tag_of() {
+    return static_cast<uint8_t>(type_index<T, AtomTypes>() + 1);
+}
+
+constexpr uint8_t EMPTY_TAG = 0;
+// vibe coded, but basically just calculates ideal size (By default i was hard coding it to 18 which isn't ideal)
+constexpr size_t ATOM_DATA_SIZE = []<size_t... I>(std::index_sequence<I...>) {
+    return std::max({ sizeof(std::tuple_element_t<I, AtomTypes>)... });
+}(std::make_index_sequence<std::tuple_size_v<AtomTypes>>{});
+
+struct AtomData {
+    alignas(16) std::byte buf[ATOM_DATA_SIZE];
+
+    template<typename T>
+    void write(const T& val) {
+        static_assert(sizeof(T) <= ATOM_DATA_SIZE);
+        std::memcpy(buf, &val, sizeof(T));
+    }
+
+    template<typename T>
+    T& read() {
+        static_assert(sizeof(T) <= ATOM_DATA_SIZE);
+        return *reinterpret_cast<T*>(buf);
+    }
+
+    template<typename T>
+    const T& read() const {
+        static_assert(sizeof(T) <= ATOM_DATA_SIZE);
+        return *reinterpret_cast<const T*>(buf);
+    }
 };
 
-// This describes some internal, human-readable functions
-template <typename T>
-struct Attatchments {
-	// Uses OR bit operator to add bit flags
-	void CreateAttatchment() {
+template<size_t Capacity>
+struct Slab {
+    struct Atom {
+        uint8_t tag = EMPTY_TAG;
+        AtomData data{};
 
-	}
-	// Shifts the bits in order to sort for more common data accesses
-	void ShiftAttatchment() {
+        template<typename T>
+        T& get() { 
+            return data.read<T>();
+        }
 
-	}
-	void ReadAttachment() {
+        template<typename T>
+        const T& get() const { 
+            return data.read<T>();
+        }
+    };
 
-	}
-	// Uses NOT bit operator to remove bit flags
-	void RemoveAttatchment() {
+    template<typename T>
+    Atom* create(const T& value) {
+        for (size_t i = 0; i < Capacity; ++i) {
+            if (!occupied[i]) {
+                occupied.set(i);
+                slots[i].tag = tag_of<T>();
+                slots[i].data.write(value);
+                active[active_count++] = i;
+                return &slots[i];
+            }
+        }
+        return nullptr;
+    }
 
-	}
+    void free(Atom* atom) {
+        size_t i = atom - slots.data();
+        occupied.reset(i);
+        slots[i].tag = EMPTY_TAG;
+        for (size_t j = 0; j < active_count; ++j) {
+            if (active[j] == i) {
+                active[j] = active[--active_count];
+                return;
+            }
+        }
+    }
+
+    // Arena allocator-style free
+    void clear() {
+        occupied.reset();
+        active_count = 0;
+    }
+
+    template<typename T, typename Fn>
+    void iter(Fn&& fn) {
+        constexpr uint8_t filter = tag_of<T>();
+        for (size_t j = 0; j < active_count; ++j) {
+            auto& slot = slots[active[j]];
+            if (slot.tag == filter)
+                fn(slot.template get<T>());
+        }
+    }
 
 private:
-	// Here we should describe what every attachment means (It's unique signature)
-	// Here we should also 
-	// Template instantiations supported
+    std::array<Atom, Capacity> slots;
+    std::bitset<Capacity> occupied;
+    std::array<size_t, Capacity> active{};
+    size_t active_count = 0;
 };
 
-// Human interface with quick access iterators, template-based api, sorting, caching and interacting with binary
-// This could also be how you instantiate data, but we might need one more manager for that, although I like
-// The idea of having the struct that creates the data for the user, also serve as everything else the user interacts with.
-struct IIterator {
+// Ugly, vibecoded functions to copy either atoms or entities
+template<size_t Capacity, size_t... I>
+typename Slab<Capacity>::Atom* clone_atom(Slab<Capacity>& slab, typename Slab<Capacity>::Atom* atom, std::index_sequence<I...>) {
+    typename Slab<Capacity>::Atom* result = nullptr;
+    ((atom->tag == tag_of<std::tuple_element_t<I, AtomTypes>>()
+        ? (result = slab.create(atom->get<std::tuple_element_t<I, AtomTypes>>()), true)
+        : false) || ...);
+    return result;
+}
 
-};
-
+template<size_t Capacity>
+std::vector<typename Slab<Capacity>::Atom*> clone_entity(Slab<Capacity>& slab, const std::vector<typename Slab<Capacity>::Atom*>& entity) {
+    std::vector<typename Slab<Capacity>::Atom*> result;
+    for (auto* atom : entity)
+        result.push_back(clone_atom(slab, atom, std::make_index_sequence<std::tuple_size_v<AtomTypes>>{}));
+    return result;
+}
--- SteamWand/SteamWand.cpp
+++ SteamWand/SteamWand.cpp
@@ -1,13 +1,50 @@
-
-
-#include <iostream>
-
 #include "Dcs.h"
+#include <vector>
 
-int main()
-{
-    DataBlock<int> db;
+struct Zombie {
+    Slab<1024>::Atom* hp;
+    Slab<1024>::Atom* speed;
+};
 
+int main() {
+    Slab<1024> slab;
 
-    std::cout << "Hello World! " << db.Data[0];
+    auto* hp = slab.create(int32_t(100));
+    auto* spd = slab.create(3.5f);
+    auto* wealth = slab.create(3);
+
+    std::vector<Slab<1024>::Atom*> zombie;
+    zombie.push_back(hp);
+    zombie.push_back(spd);
+    zombie.push_back(wealth);
+
+    std::cout << "Normal zombie wealth: " << zombie[2]->get<int32_t>() << std::endl;
+
+    std::vector<Slab<1024>::Atom*> super_zombie = clone_entity(slab, zombie);
+    super_zombie[2]->get<int32_t>() = 5;
+    std::cout << "Super zombie wealth updated: " << super_zombie[2]->get<int32_t>() << std::endl;
+
+    slab.create(Vec3{ 1, 2, 3 });
+    slab.create(Vec3{ 7, 0, 5 });
+    slab.create(Vec2{ 9, 4 });
+
+    std::cout << "speed: " << spd->get<float>() << "\n";
+
+    std::cout << "\n=== Vec3 only ===\n";
+    slab.iter<Vec3>([](Vec3& v) {
+        std::cout << v.x << "," << v.y << "," << v.z << "\n";
+        });
+
+    std::cout << "\n=== Vec2 only ===\n";
+    slab.iter<Vec2>([](Vec2& v) {
+        std::cout << v.x << "," << v.y << "\n";
+        });
+
+    auto find = [](auto& vec, uint8_t tag) -> Slab<1024>::Atom* {
+        for (auto* a : vec)
+            if (a->tag == tag) return a;
+        return nullptr;
+        };
+
+    slab.free(hp);
 }
